002f4f1a6984da32a4ec1c236397b052c2dc53af
Esteban Monge <estebanmonge@riseup.net>
2022-09-09 13:39:05 -0600

    add a field to use a different phone number

--- static/src/js/pos_receipt_image.js
+++ static/src/js/pos_receipt_image.js
@@ -19,6 +19,42 @@ odoo.define("point_of_sale_logo.pos_receipt_image", function (require) {
                     }
                 }
             }
+            get isSelfInformation() {
+                if (this.env.pos){
+                    if (this.env.pos.config){
+                            if (this.env.pos.config.pos_enable_self_information != false){
+                                return true;
+                            }
+                            else {
+                                return false;
+                            }
+                    }
+                }
+            }
+            get selfPhone() {
+                if (this.env.pos){
+                    if (this.env.pos.config){
+                            if (this.env.pos.config.pos_phone != false){
+                                return this.env.pos.config.pos_phone;
+                            }
+                            else {
+                                return false;
+                            }
+                    }
+                }
+            }
+            get selfName() {
+                if (this.env.pos){
+                    if (this.env.pos.config){
+                            if (this.env.pos.config.pos_name != false){
+                                return this.env.pos.config.pos_name;
+                            }
+                            else {
+                                return false;
+                            }
+                    }
+                }
+            }
         };
     Registries.Component.extend(OrderReceipt, PosReceiptLogoChrome);
     return OrderReceipt;
